c1c370293e7b984eed6f5d1ff4d65ce02d5cb9a7
Francisco Calderón <fjcalderon@gmail.com>
2022-12-29 10:48:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Mostro CLI
+
+Very simple command line interface that show all new replaceable events from [Mostro](https://github.com/MostroP2P/mostro)
+
+## Requirements:
+
+0. You need Rust version 1.64 or higher to compile.
+1. You will need a lightning network node
+
+## Install dependencies:
+
+To compile on Ubuntu/Pop!\_OS, please install [cargo](https://www.rust-lang.org/tools/install), then run the following commands:
+
+```
+$ sudo apt update
+$ sudo apt install -y cmake build-essential pkg-config
+```
+
+## Install
+
+```
+$ git clone https://github.com/MostroP2P/mostro-cli.git
+$ cd mostro-cli
+$ cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use clap::Parser;
+use nostr::util::time::timestamp;
+use nostr::{key::FromBech32, Keys};
+use nostr::{Kind, SubscriptionFilter};
+use nostr_sdk::{Client, RelayPoolNotifications, Result};
+
+/// cli arguments
+#[derive(Parser, Debug)]
+#[clap(author = "Francisco Calderón", version, about)]
+/// Mostro P2P cli client
+struct Arguments {
+    list: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // TODO: handle arguments
+    // let args = Arguments::parse();
+
+    // mostro pubkey
+    let pubkey = "npub1m0str0n64lfulw5j6arrak75uvajj60kr024f5m6c4hsxtsnx4dqpd9ape";
+    let mostro_keys = nostr::key::Keys::from_bech32_public_key(pubkey)?;
+
+    // Generate new keys
+    let my_keys: Keys = Client::generate_keys();
+    // Create new client
+    let client = Client::new(&my_keys);
+
+    // Add relays
+    client.add_relay("wss://nostr.fly.dev", None).await?;
+    client
+        .add_relay("wss://relay.cryptocculture.com", None)
+        .await?;
+
+    // Connect to relays and keep connection alive
+    client.connect().await?;
+
+    let subscription = SubscriptionFilter::new()
+        .author(mostro_keys.public_key())
+        .since(timestamp());
+
+    client.subscribe(vec![subscription]).await?;
+
+    // Handle notifications
+    loop {
+        let mut notifications = client.notifications();
+        while let Ok(notification) = notifications.recv().await {
+            if let RelayPoolNotifications::ReceivedEvent(event) = notification {
+                if let Kind::Custom(kind) = event.kind {
+                    if kind >= 10000 && kind < 20000 {
+                        println!("Replaceable event: {:#?}", event);
+                    }
+                }
+            }
+        }
+    }
+}
