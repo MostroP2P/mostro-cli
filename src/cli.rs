0bdd9e6a4e590f96ef681ed8408e64e06a4f970b
Francisco Calderón <fjcalderon@gmail.com>
2023-11-28 10:22:35 -0300

    Removes unreachable arm

--- src/cli.rs
+++ src/cli.rs
@@ -254,17 +254,6 @@ pub async fn run() -> Result<()> {
                 )
                 .await?
             }
-            Commands::AdmAddSolver { npubkey } => {
-                execute_send_msg(
-                    cmd.clone(),
-                    None,
-                    &my_key,
-                    mostro_key,
-                    &client,
-                    Some(npubkey),
-                )
-                .await?
-            }
             Commands::Neworder {
                 kind,
                 fiat_code,
