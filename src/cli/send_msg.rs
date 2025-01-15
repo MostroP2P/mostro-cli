8cf98323486f0d8bc8f92a3a5f90f9ae18a4259c
Francisco Calderón <fjcalderon@gmail.com>
2025-01-15 16:20:03 -0300

    Fix canceling orders

--- src/cli/send_msg.rs
+++ src/cli/send_msg.rs
@@ -148,19 +148,36 @@ async fn process_order_response(
     trade_keys: &Keys,
     request_id: u64,
 ) -> Result<()> {
-    if let Some(order) = dm.iter().find_map(|el| {
-        let message = el.0.get_inner_message_kind();
-        if message.request_id == Some(request_id) {
-            if let Some(Payload::Order(order)) = message.payload.as_ref() {
-                return Some(order.clone());
+    for (message, _) in dm {
+        let kind = message.get_inner_message_kind();
+        if let Some(req_id) = kind.request_id {
+            if req_id != request_id {
+                continue;
+            }
+
+            match kind.action {
+                Action::NewOrder => {
+                    if let Some(Payload::Order(order)) = kind.payload.as_ref() {
+                        if let Err(e) =
+                            Order::new(pool, order.clone(), trade_keys, Some(request_id as i64))
+                                .await
+                        {
+                            println!("Failed to create new order: {}", e);
+                        }
+                        return Ok(());
+                    }
+                }
+                Action::Canceled => {
+                    if let Some(id) = kind.id {
+                        if let Err(e) = Order::delete_by_id(pool, &id.to_string()).await {
+                            println!("Failed to delete order: {}", e);
+                        }
+                        return Ok(());
+                    }
+                }
+                _ => (),
             }
         }
-        None
-    }) {
-        println!("Order id {} created", order.id.unwrap());
-        Order::new(pool, order.clone(), trade_keys, Some(request_id as i64)).await?;
-    } else {
-        println!("Error: No matching order found in response");
     }
 
     Ok(())
