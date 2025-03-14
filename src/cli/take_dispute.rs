73c6b9e2e69518843a9298124061714e10c0caa2
arkanoider <github.913zc@simplelogin.com>
2025-03-14 22:01:42 +0100

    Fixed admin keys send in case of admin dispute commands

--- src/cli/take_dispute.rs
+++ src/cli/take_dispute.rs
@@ -5,6 +5,70 @@ use uuid::Uuid;
 
 use crate::util::send_message_sync;
 
+pub async fn execute_admin_cancel_dispute(
+    dispute_id: &Uuid,
+    identity_keys: &Keys,
+    trade_keys: &Keys,
+    mostro_key: PublicKey,
+    client: &Client,
+) -> Result<()> {
+    println!(
+        "Request of cancel dispute {} from mostro pubId {}",
+        dispute_id,
+        mostro_key.clone()
+    );
+    // Create takebuy message
+    let take_dispute_message =
+        Message::new_dispute(Some(*dispute_id), None, None, Action::AdminCancel, None);
+
+    println!("identity_keys: {:?}", identity_keys.public_key.to_string());
+
+    send_message_sync(
+        client,
+        Some(identity_keys),
+        trade_keys,
+        mostro_key,
+        take_dispute_message,
+        true,
+        false,
+    )
+    .await?;
+
+    Ok(())
+}
+
+pub async fn execute_admin_settle_dispute(
+    dispute_id: &Uuid,
+    identity_keys: &Keys,
+    trade_keys: &Keys,
+    mostro_key: PublicKey,
+    client: &Client,
+) -> Result<()> {
+    println!(
+        "Request of take dispute {} from mostro pubId {}",
+        dispute_id,
+        mostro_key.clone()
+    );
+    // Create takebuy message
+    let take_dispute_message =
+        Message::new_dispute(Some(*dispute_id), None, None, Action::AdminSettle, None);
+
+    println!("identity_keys: {:?}", identity_keys.public_key.to_string());
+
+    send_message_sync(
+        client,
+        Some(identity_keys),
+        trade_keys,
+        mostro_key,
+        take_dispute_message,
+        true,
+        false,
+    )
+    .await?;
+
+    Ok(())
+}
+
 pub async fn execute_take_dispute(
     dispute_id: &Uuid,
     identity_keys: &Keys,
@@ -25,6 +89,8 @@ pub async fn execute_take_dispute(
         Action::AdminTakeDispute,
         None,
     );
+
+    println!("identity_keys: {:?}", identity_keys.public_key.to_string());
 
     send_message_sync(
         client,
