9cf93456fe413fff93058673d1de39c298cfa4b4
arkanoider <github.913zc@simplelogin.com>
2025-10-23 22:32:27 +0200

    fix: correct receiver of restore session message

--- src/cli/restore.rs
+++ src/cli/restore.rs
@@ -23,7 +23,7 @@ pub async fn execute_restore(
     let sent_message = send_dm(
         &ctx.client,
         Some(identity_keys),
-        identity_keys,
+        &ctx.trade_keys,
         &mostro_key,
         message_json,
         None,
@@ -49,10 +49,10 @@ pub async fn execute_restore(
     println!("⏳ Recovering pending orders and disputes...\n");
 
     // Wait for incoming DM
-    let recv_event = wait_for_dm(ctx, Some(identity_keys), sent_message).await?;
+    let recv_event = wait_for_dm(ctx, Some(&ctx.trade_keys), sent_message).await?;
 
     // Parse the incoming DM
-    let messages = parse_dm_events(recv_event, identity_keys, None).await;
+    let messages = parse_dm_events(recv_event, &ctx.trade_keys, None).await;
     if let Some((message, _, _)) = messages.first() {
         let message = message.get_inner_message_kind();
         if message.action == Action::RestoreSession {
